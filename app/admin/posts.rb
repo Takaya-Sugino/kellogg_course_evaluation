e41d7ffe97183dd224a1584a6d2d6dda311208bf
anonymous anonymous <takaya.sugino@kellogg.northwestern.edu>
2022-03-15 16:25:33 +0000

    Whitelist columns in Post for ActiveAdmin

--- app/admin/posts.rb
+++ app/admin/posts.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Post do
 
+ permit_params :professor, :rating, :description, :course_id, :poster_id
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
