c33468a1e76c92296009484603e85cc6769bc319
anonymous anonymous <takaya.sugino@kellogg.northwestern.edu>
2022-03-15 16:25:31 +0000

    Whitelist columns in Course for ActiveAdmin

--- app/admin/courses.rb
+++ app/admin/courses.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Course do
 
+ permit_params :number, :name, :core_elective
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
