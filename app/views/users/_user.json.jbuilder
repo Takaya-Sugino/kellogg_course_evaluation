1153458c695e50f2a8baa6d95614a9f41444eb24
anonymous anonymous <takaya.sugino@kellogg.northwestern.edu>
2022-03-15 16:25:21 +0000

    Generate users resource with ideas:scaffold

---
+++ app/views/users/_user.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! user, :id, :name, :program, :graduation_year, :interest, :created_at, :updated_at
+json.url user_url(user, format: :json)
---
+++ db/migrate/20220315162520_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :program
+      t.string :graduation_year
+      t.string :interest
+
+      t.timestamps
+    end
+  end
+end
